953466245a923a728632e388e4281dd2f6a48d81
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2021-01-22 14:39:00 -0500

    style and login page setup

---
+++ script.js
@@ -0,0 +1,30 @@
+// Users need to be able to deposit money into an account, savings and checking
+
+const currency = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  // this creates language sensitive number formatting, this will allow users to enter in their proper currency. In this case it will be USD.
+})
+
+
+// Users need to be able to see their balance in their savings and checking account
+
+
+
+// Users need to be able to withdraw from savings and checking account
+
+
+
+
+
+// Account cannot not be negative 
+
+
+
+// Accounts that hit zero show a red 0
+
+
+
+
+// Account needs to function independently 
