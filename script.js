fff624bc2b9ae7be97d2264517bccf6b5b8837c9
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2021-01-22 15:17:53 -0500

    grabbed dom elements

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-// Users need to be able to deposit money into an account, savings and checking
+// Grab deposit buttons from the DOM
+const checkingWithdrawl = document.querySelector('#withdraw-checking')
+const checkingDeposit = document.querySelector('#deposit-checking')
+
+// Grab withdraw buttons from the DOM
+const savingsWithdrawl = document.querySelector('#withdraw-savings')
+const savingsDeposit = document.querySelector('#deposit-savings')
+
+//Check buttons if working
+console.log(checkingWithdrawl)
+console.log(checkingDeposit)
+console.log(savingsWithdrawl)
+console.log(savingsDeposit)
+
+// Users need to be able to deposit money into an account, savings and checking.
+const deposit = [] //establish a variable with an empty array, this is what user will put in.
+
+// Users need to be able to withdraw from savings and checking account
+const withdraw = [] //establish a variable with an empty array, this is what user take out.
+
+// Users need to be able to see their balance in their savings and checking account
+const checkingBalance = document.querySelector('#checking-balance')
+const savingsBalance = document.querySelector('#savings-balance')
+
+//Check buttons if working
+console.log(checkingBalance)
+console.log(savingsBalance)
+
+
+
 
 const currency = new Intl.NumberFormat('en-US', {
   style: 'currency',
@@ -6,15 +35,6 @@ const currency = new Intl.NumberFormat('en-US', {
   minimumFractionDigits: 2,
   // this creates language sensitive number formatting, this will allow users to enter in their proper currency. In this case it will be USD.
 })
-
-
-// Users need to be able to see their balance in their savings and checking account
-
-
-
-// Users need to be able to withdraw from savings and checking account
-
-
 
 
 
